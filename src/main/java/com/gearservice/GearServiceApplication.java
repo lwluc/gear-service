ac6d75c964c1f814c984a672aa13c85232463066
Luc Weinbrecht <luc.weinbrecht@novatec-gmbh.de>
2022-07-15 11:48:48 +0200

    using demo data on all profiles

--- src/main/java/com/gearservice/GearServiceApplication.java
+++ src/main/java/com/gearservice/GearServiceApplication.java
@@ -9,15 +9,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Profile;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import java.util.Collections;
 import java.util.HashSet;
 
-import static java.util.Arrays.asList;
 import static java.util.Collections.singletonList;
 
 /**
@@ -40,7 +37,6 @@ public class GearServiceApplication {
     public static void main(String[] args) {SpringApplication.run(GearServiceApplication.class, args);}
 
     @Bean
-    @Profile({"dev", "docker"})
     CommandLineRunner init() {
         final String adminUser = "admin";
         return args -> {
